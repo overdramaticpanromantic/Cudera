d33cfa374411b55ffc7f9d17d4e61fd77933047a
overdramaticpanromantic <fkorogy@icloud.com>
2024-07-31 19:12:13 -0400

    More items

--- src/cudera/content/CuderaItems.java
+++ src/cudera/content/CuderaItems.java
@@ -5,7 +5,7 @@ import cudera.graphics.CuderaPalette;
 import mindustry.type.Item;
 
 public class CuderaItems {
-    public static Item cyanomite, leucoferrite, polysomate, silt, anthracite, vitrinite;
+    public static Item cyanomite, leucoferrite, polysomate, silt, anthracite, aragonite, quartz, algae, vitrinite;
     public static final Seq<Item> cuderaItems = new Seq<>();
     public static void load(){
         cyanomite = new Item("cyanomite"){{
@@ -27,19 +27,38 @@ public class CuderaItems {
         silt = new Item("silt"){{
             color = CuderaPalette.siltLight;
             hardness = 1;
+            lowPriority = true;
         }};
 
         anthracite = new Item("anthracite"){{
             color = CuderaPalette.anthraciteLight;
+            cost = 0.8f;
+            flammability = 0.75f;
+        }};
+
+        aragonite = new Item("aragonite"){{
+            color = CuderaPalette.aragoniteLight;
+            hardness = 2;
+        }};
+
+        quartz = new Item("quartz"){{
+            color = CuderaPalette.quartzLight;
+            cost = 0.9f;
+        }};
+
+        algae = new Item("algae"){{
+            color = CuderaPalette.algaeLight;
+            flammability = 0.5f;
         }};
 
         vitrinite = new Item("vitrinite"){{
             color = CuderaPalette.vitriniteLight;
             hardness = 2;
+            flammability = 1f;
         }};
 
         cuderaItems.addAll(
-            cyanomite, leucoferrite, polysomate, silt, anthracite, vitrinite
+            cyanomite, leucoferrite, polysomate, silt, anthracite, aragonite, quartz, algae, vitrinite
         );
     }
 }
--- src/cudera/graphics/CuderaPalette.java
+++ src/cudera/graphics/CuderaPalette.java
@@ -5,10 +5,12 @@ import arc.graphics.Color;
 public class CuderaPalette {
     public static Color
     baseLight = Color.valueOf("c7dbcf"),
-    baseOutline = Color.valueOf("2c3b3c"),
     polysomateLight = Color.valueOf("c8b38e"),
     leucoferriteLight = Color.valueOf("ffbacd"),
     siltLight = Color.valueOf("bcc2d3"),
     anthraciteLight = Color.valueOf("bcbfad"),
+    aragoniteLight = Color.valueOf("ea8c64"),
+    quartzLight = Color.valueOf("ee98db"),
+    algaeLight = Color.valueOf("53d56d"),
     vitriniteLight = Color.valueOf("b2db9f");
 }
