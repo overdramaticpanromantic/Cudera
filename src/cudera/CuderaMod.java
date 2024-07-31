4f1868a09ea2f69f37fbf971097bb9a28e4a36a2
overdramaticpanromantic <fkorogy@icloud.com>
2024-07-31 13:19:03 -0400

    I hope I actually fixed it this time

--- src/cudera/CuderaMod.java
+++ src/cudera/CuderaMod.java
@@ -1,11 +1,17 @@
 package cudera;
 
+import arc.util.Log;
+import cudera.content.*;
 import mindustry.mod.*;
 import cudera.gen.*;
 
 public class CuderaMod extends Mod{
     @Override
-    public void loadContent(){
+    public void loadContent()
+    {
+        Log.info("Loaded Cudera content.");
         EntityRegistry.register();
+        CuderaItems.load();
+        CuderaBlocks.load();
     }
 }
